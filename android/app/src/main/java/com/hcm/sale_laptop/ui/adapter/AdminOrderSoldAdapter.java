fedaecf891e7eb49561d22fab4ebe0907f8ea07d
NghiaIOT <61870294+NghiaIOT@users.noreply.github.com>
2024-11-30 10:39:25 +0700

    Revert "add: adpi"
    This reverts commit 67030c566fbad4cf84f96427ea9e3372cd1ab2fe.

--- android/app/src/main/java/com/hcm/sale_laptop/ui/adapter/AdminOrderSoldAdapter.java
+++ android/app/src/main/java/com/hcm/sale_laptop/ui/adapter/AdminOrderSoldAdapter.java
@@ -1,60 +1,28 @@
 package com.hcm.sale_laptop.ui.adapter;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.hcm.sale_laptop.R;
-import com.hcm.sale_laptop.data.model.other.OrderSoldModel;
+import com.hcm.sale_laptop.data.model.other.ProductModel;
 
 import java.util.List;
 
-public class AdminOrderSoldAdapter extends RecyclerView.Adapter<AdminOrderSoldAdapter.OrderViewHolder> {
-    private final List<OrderSoldModel> orderList;
+import com.hcm.base.BaseAdapter;
+import com.hcm.base.OnItemClick;
+import com.hcm.sale_laptop.databinding.ItemOrderSoldBinding;
 
-    public AdminOrderSoldAdapter(List<OrderSoldModel> orderList) {
-        this.orderList = orderList;
-    }
+public class AdminOrderSoldAdapter extends BaseAdapter<ProductModel, ItemOrderSoldBinding> {
 
-    @NonNull
-    @Override
-    public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order_sold, parent, false);
-        return new OrderViewHolder(view);
+    public AdminOrderSoldAdapter(List<ProductModel> itemList, OnItemClick<ProductModel> listener) {
+        super(itemList, listener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull OrderViewHolder holder, int position) {
-        OrderSoldModel order = orderList.get(position);
-        holder.tvOrderId.setText("Mã đơn hàng: " + order.getOrderId());
-        holder.tvProductName.setText(order.getProductName());
-        holder.tvPrice.setText(order.getPrice() + " VND");
-        holder.tvDetails.setText("Số lượng: " + order.getQuantity() + " | Thời gian: " + order.getDate());
-        // Set hình ảnh nếu cần (ở đây dùng ảnh mặc định)
-        holder.orderImage.setImageResource(order.getQuantity()); // Thay `ic_order` bằng hình thực tế
+    protected ItemOrderSoldBinding createBinding(LayoutInflater inflater, ViewGroup parent) {
+        return ItemOrderSoldBinding.inflate(inflater, parent, false);
     }
 
     @Override
-    public int getItemCount() {
-        return orderList.size();
-    }
+    protected void bindData(ProductModel item, ItemOrderSoldBinding binding, int position) {
 
-    public static class OrderViewHolder extends RecyclerView.ViewHolder {
-        TextView tvOrderId, tvProductName, tvPrice, tvDetails;
-        ImageView orderImage;
-
-        public OrderViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvOrderId = itemView.findViewById(R.id.tvMaDonHang);
-            tvProductName = itemView.findViewById(R.id.tvNameProduct);
-            tvPrice = itemView.findViewById(R.id.tvPrice);
-            tvDetails = itemView.findViewById(R.id.tvQuantity);
-            orderImage = itemView.findViewById(R.id.orderImage);
-        }
     }
 }
