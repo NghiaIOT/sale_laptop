abff45dffffa58cafb0cb22a98f2617b886ca6ca
NghiaIOT <61870294+NghiaIOT@users.noreply.github.com>
2024-11-30 10:47:16 +0700

    adnroid : refactor code

--- android/app/src/main/java/com/hcm/sale_laptop/data/api/RetrofitClient.java
+++ android/app/src/main/java/com/hcm/sale_laptop/data/api/RetrofitClient.java
@@ -16,7 +16,7 @@ public class RetrofitClient {
     // local host for simulator
 //    public static final String BASE_URL = "http://10.0.2.2:3000/";
 
-    public static final String BASE_URL = "https://3539-171-252-155-71.ngrok-free.app";
+    public static final String BASE_URL = "https://cf11-2402-800-6294-4913-751c-31fb-d302-aa10.ngrok-free.app";
 
     private static Retrofit retrofit;
 
